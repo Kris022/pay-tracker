ec7159f1f3b734b3e9b9007f8e59efd6543d9aef
Kris <kharonek@yahoo.com>
2022-06-28 15:56:36 -0300

    some bootstrap

--- home.php
+++ home.php
@@ -4,18 +4,16 @@
 #  include("includes/sidenav.php");
 ?>
 
-<div class="container">
+<div class="container-fluid my-custom-container">
+    <div class="row my-custom-row justify-content-center align-items-center">
+      <div class="col-lg-3 m-1 col-sm-12"><div class="p-3 border bg-light"><i class="fa-solid fa-bars"></i></div></div>
+      <div class="col-lg-3 m-1 col-sm-12"><div class="p-3 border bg-light"><i class="fas fa-chalkboard-teacher"></i></div></div>
+    </div>
 
-  <div class="row">
-    <div class="col-lg-3 col-sm-12"><div class="p-3 border bg-light"><i class="fa-solid fa-bars"></i></div></div>
-    <div class="col-lg-3 col-sm-12"><div class="p-3 border bg-light"><i class="fas fa-chalkboard-teacher"></i></div></div>
-  </div>
-
-  <div class="row my-custom-row justify-content-center align-items-center">
-    <div class="col-lg-3 col-sm-12"><div class="p-3 border bg-light"><i class="fas fa-table"></i></div></div>
-    <div class="col-lg-3 col-sm-12"><div class="p-3 border bg-light"><i class="fas fa-users"></i></div></div>
-  </div>
-
+    <div class="row my-custom-row justify-content-center align-items-center">
+      <div class="col-lg-3 m-1 col-sm-12"><div class="p-3 border bg-light"><i class="fas fa-table"></i></div></div>
+      <div class="col-lg-3 m-1 col-sm-12"><div class="p-3 border bg-light"><i class="fas fa-users"></i></div></div>
+    </div>
 </div>
 
 
